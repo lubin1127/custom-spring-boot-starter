9bfb9732b2b0e43fc479767bfc4a6fd4ebf964c9
lubin <1059109685@qq.com>
2021-02-25 14:36:43 +0800

    web-spring-boot-starter

---
+++ web-spring-boot-starter/src/main/java/me/lubin/web/starter/config/DefaultDispatcherConfig.java
@@ -0,0 +1,30 @@
+package me.lubin.web.starter.config;
+
+import me.lubin.web.starter.MyDefaultErrorAttributes;
+import me.lubin.web.starter.MyDefaultHandlerExceptionResolver;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author lubin
+ * @create 2021.01.28
+ */
+@Configuration
+public class DefaultDispatcherConfig {
+
+    @Bean
+    public ErrorAttributes errorAttributes() {
+        return new MyDefaultErrorAttributes();
+    }
+
+    @Bean
+    public MyDefaultHandlerExceptionResolver myDefaultHandlerExceptionResolver() {
+        Set<Class<? extends Exception>> customExceptionSet = new HashSet<>();
+        return new MyDefaultHandlerExceptionResolver(customExceptionSet);
+    }
+
+}
---
+++ web-spring-boot-starter/src/main/java/me/lubin/web/starter/consts/WebKeyConsts.java
@@ -0,0 +1,21 @@
+package me.lubin.web.starter.consts;
+
+/**
+ * @author lubin
+ * @create 2020.12.14
+ */
+public interface WebKeyConsts {
+
+    int STACK_TRACE = 1;
+
+    String STATUS = "status";
+
+    String MESSAGE = "message";
+
+    String RESULT = "result";
+
+    String DATA = "data";
+
+    String MSG = "msg";
+
+}
